fe792132d2b783faf30034f407b97b7c27f8f14a
Jose Del Rio <jose.sanz@reachplc.com>
2020-03-28 17:48:45 +0000

    Initial commit

---
+++ src/sources.py
@@ -0,0 +1,12 @@
+
+
+data_sources = {
+    'spain': [
+        {'url': 'https://covid19.isciii.es/resources/serie_historica_acumulados.csv',
+         'type': 'csv',
+         'desc': 'Cases by date and Region (Infected, Hospitalized, Intensive Care Unit & Deaths)'}
+    ]
+}
+
+
+countries = data_sources.keys()
